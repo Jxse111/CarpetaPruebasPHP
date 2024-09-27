7a87e12075ae1a59ba1cb026c5e3e2fd38f7512b
José Martínez <116352794+Jxse111@users.noreply.github.com>
2024-09-27 09:37:20 +0200

    Add files via upload

--- UD1/Pruebas.php
+++ UD1/Pruebas.php
@@ -71,11 +71,49 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         </p>
 
         <p> Ejemplo de cadena de texto con comillas simples:
-            <?php $nombre = "Juan";
-            echo $cad1 = 'Hola,$nombre'; ?>    
+            <?php
+            $nombre = "Juan";
+            echo $cad1 = 'Hola,$nombre';
+            ?>    
         <p/>
         <p>Ejemplo de cadena de texto con comillas dobles: 
             <?php echo $cad2 = "Hola,$nombre"; ?>
         <p>
+        <p>Ejemplo de concatenación, a diferencia de java se utiliza el .(punto) y no el método concat.</p>
+        <?php
+        $a = "Hola";
+        $b = $a . ",soy José";
+        echo $b;
+        ?>
+        <br/>
+        <p>Ejemplo de el uso de heredoc:</p>
+        <?php
+        $a = <<<MICADENA
+            Ejemplo de Prueba con un heredoc (<<<(nombreDeLaVariable))<br/>
+                    
+            Desarrollo de Aplicaciones Web<br />
+
+            Desarrollo Web en Entorno Servidor
+
+            MICADENA;
+
+        print $a;
+        ?> 
+
+        <p>Ejemplo de settype y gettype</p>
+        <?php
+        $a = $b = "3.1416"; // asignamos a las dos variables la misma cadena de texto
+
+        settype($b, "float"); // y cambiamos $b a tipo float
+
+        print "\$a vale $a y es de tipo " . gettype($a);
+
+        print "<br />";
+
+        print "\$b vale $b y es de tipo " . gettype($b);
+        ?> 
+
+
+
     </body>
 </html>
